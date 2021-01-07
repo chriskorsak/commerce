e71294d770b7fd939b50e09ce4c1995d0fe9117b
chriskorsak <chris@chriskorsak.com>
2021-01-06 17:47:10 -0800

    add related names to model attributes, add category attribute to Listing model.

--- auctions/models.py
+++ auctions/models.py
@@ -2,7 +2,6 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
-  # watchlist = models.ManyToManyField(Listing)
 
   def __str__(self):
     return f"{self.username} ({self.first_name} {self.last_name})"
@@ -11,20 +10,20 @@ class User(AbstractUser):
 
 class Listing(models.Model):
   # foreign key refers to the User object
-  user = models.ForeignKey(User, on_delete=models.CASCADE)
-
+  user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
   title = models.CharField(max_length=64)
   price = models.DecimalField(max_digits=8, decimal_places=2)
   description = models.TextField(max_length=256)
   date = models.DateField(auto_now_add=True)
   photo = models.URLField(max_length=200, blank=True)
+  category = models.CharField(max_length=32, blank=True)
 
   # returns a string representation of the Listing object
   def __str__(self):
     return f"User:{self.user} Item:{self.title} Price:{self.price} Date Listed:{self.date}"
 
 class Bid(models.Model):
-  listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+  listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="bids")
   price = models.DecimalField(max_digits=8, decimal_places=2)
   bidder = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
@@ -35,7 +34,7 @@ class Comment(models.Model):
   user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
   date = models.DateField(auto_now_add=True)
   comment = models.CharField(max_length=256)
-  listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+  listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="comments")
 
   def __str__(self):
     return f"Date:{self.date} Comment:{self.comment} Listing:{self.listing}"
