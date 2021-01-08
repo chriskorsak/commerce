01e3237a49b3ebe52145bbb69f1c746f1c63ef8e
chriskorsak <chris@chriskorsak.com>
2021-01-07 19:27:07 -0800

    create forms.py, CreateListing class for use in django forms. class not implemented yet.

---
+++ auctions/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+#list form classes here
+class CreateEntryForm(forms.Form):
+  title = forms.CharField(max_length=64, initial="Title")
+  price = forms.DecimalField(max_digits=8, decimal_places=2, initial="Price")
+  description = forms.CharField(widget=forms.Textarea, max_length=256, initial="Description")
+  photo = forms.CharField(max_length=200, initial="Photo URL")
+  category = forms.CharField(max_length=32, initial="Category")
